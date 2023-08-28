beb2a4ab4aa2fdb84d3187765cc8d40181ad5ecc
Aimee Gibson <aimjo617@gmail.com>
2023-08-28 17:51:39 -0600

    updated

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["SECRET_KEY"] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def home_page():
-    return render_template("home.html")
+    """Generate and show form to ask for prompts"""
+    prompts = story.prompts
+    return render_template("home.html", prompts=prompts)
+
+@app.route("/story")
+def show_story():
+    """Show story result."""
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
 
 """Madlibs Stories."""
 
